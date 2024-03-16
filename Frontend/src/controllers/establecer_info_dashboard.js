28d928e164570da16b34f0ce7ec92c978e8fdd94
stiven <stivenchoo@gmail.com>
2024-03-16 16:35:48 -0500

    compare encrypt password with password input

--- Frontend/src/controllers/establecer_info_dashboard.js
+++ Frontend/src/controllers/establecer_info_dashboard.js
@@ -1,6 +1,5 @@
 
 let usuario = JSON.parse(localStorage.getItem('user'))
-
 console.log(usuario)
 const name = document.querySelector('#name-user-box').textContent = usuario.nombre
 const email = document.querySelector('#email-user-box').textContent = usuario.correo
